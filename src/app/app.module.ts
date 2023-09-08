798ac235b90494877df02b247a919e1818f56af3
kmmasden <kaitmasdes@gmail.com>
2023-09-08 12:10:40 -0400

    created background component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { MenuComponent } from './menu/menu.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    MenuComponent
+    MenuComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
